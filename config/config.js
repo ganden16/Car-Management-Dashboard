d2c422edf036ce68c946026697ae0dd1675f4ccf
ganden16 <alvindian85@gmail.com>
2023-06-11 23:43:24 +0700

    update:.sequelizerc

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+	development: {
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		dialect: "postgres",
+		timezone: "+07:00"
+	},
+
+	production: {
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		dialect: "postgres",
+		timezone: "+07:00"
+	}
+}
